7b6ecf046949d7d9e1b33ec1cfec374c5010ca90
Xantiem <nickspexx@gmail.com>
2024-05-19 23:16:49 +0200

    some code

--- test.c
+++ test.c
@@ -2,8 +2,21 @@
 
 #include "matrix.h"
 
-int main() {
+int matrix(int arr, int size) {
 
+    return sizeof(arr) / size;
+}
+
+int matrix_gen (unsigned short int creation_size) {
+
+}
+
+int main() {
+    int size[] = {2,2};
+
+    
+
+    printf("%d", length);
 
     return 0;
 }
