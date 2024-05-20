c357343c28278fab1dd17222bf44259dec339abe
Xantiem <nickspexx@gmail.com>
2024-05-20 20:08:05 +0200

    ideas

--- test.c
+++ test.c
@@ -2,21 +2,25 @@
 
 #include "matrix.h"
 
+int matrix_flattern(int *arr, int *size) {
+    for(int count=0; count < (sizeof(arr) / sizeof(int)); count++) {
+        printf("%d", count);
+    }
+}
+
 int matrix(int arr, int size) {
 
     return sizeof(arr) / size;
 }
-
 int matrix_gen (unsigned short int creation_size) {
 
 }
 
 int main() {
-    int size[] = {2,2};
+    int size[] = {2};
 
-    
-
-    printf("%d", length);
+    int x[3][3] = {{1,1,2},{3,3,4}};
+    matrix_flattern(&x, size);
 
     return 0;
 }
